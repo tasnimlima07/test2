dd9aa86b94207234b2bf14767db3acd5d61bffad
tasnimlima07 <tasnimlima07@gmail.com>
2023-08-20 14:51:52 +0600

    Your commit message here

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 }
 
 database_url =os.environ.get("DATABASE_URL")
-DATABASES["default"]= dj_database_url.parse("database_url")
+DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
