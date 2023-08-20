b5f492d2e015821f223ffcea7978743b57ca05fb
tasnimlima07 <tasnimlima07@gmail.com>
2023-08-20 15:43:57 +0600

    url

--- resume_app/urls.py
+++ resume_app/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
-from 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
@@ -31,5 +31,10 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
